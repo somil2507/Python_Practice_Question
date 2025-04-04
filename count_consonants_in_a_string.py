37b716dfacd575213c299cb23bf0a695ead1d776
somil2507 <72187232+somil2507@users.noreply.github.com>
2025-04-04 11:23:59 +0530

    Update count_consonants_in_a_string.py

--- count_consonants_in_a_string.py
+++ count_consonants_in_a_string.py
@@ -1,18 +1,22 @@
-from itertools import count
-
-
-def count_consonants_in_a_string():
+def count_consonants():
 
     vowels = ['a', 'e', 'i', 'o', 'u']
-
     count = 0
 
-    string1 = input("Enter a String: ", ).lower()
+    string_input = input("Enter a String : ", ).lower()
 
-    for i in string1:
+    new_string = []
+
+    
+    for word in string_input:
+        if word.isalpha():
+            new_string.append(word)
+
+    for i in new_string:
         if i not in vowels:
             count = count + 1
 
-    print(count) 
+    print("Total Consonants are : ", count)
 
-count_consonants_in_a_string()           
+
+count_consonants()
